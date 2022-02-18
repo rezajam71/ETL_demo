8ccf231e4b61c6565ebc25e8f4c66329f0256e11
rezajam71 <rezajam71@yahoo.com>
2022-02-18 19:58:53 +0330

    add table

--- digital.py
+++ digital.py
@@ -8,30 +8,29 @@ a_file = open("AU_DigitalVideo_MM.csv")
 a_file.seek(408)
 rows = csv.reader(a_file,delimiter=';')
 c.execute(''' CREATE TABLE IF NOT EXISTS Digital_Video(
-            Week Ending int,
+            Week_Ending int,
             Brand txt,
-            Sub Brand txt,
+            Sub_Brand txt,
             Variant txt,
             Website txt,
-            Campaign Name txt,
+            Campaign_Name txt,
             Creative txt,
-            Copy Name txt,
-            Adss Type txt,
-            Ads Format txt,
+            Copy_Name txt,
+            Adss_Type txt,
+            Ads_Format txt,
             Platform txt,
-            Thematical Tactical txt,
-            SCJ Sub Two  New launch txt,
-            Buying Type txt,
+            Thematical_Tactical txt,
+            SCJ_Sub_Two_New_launch txt,
+            Buying_Type txt,
             Views integer,
             Clicks integer,
             Impressions integer,
-            Actual Spending integer,
-            Spend USD real
+            Actual_Spending real,
+            Spend_In_USD real
                 )''')
 #c.executemany("INSERT INTO Digital_Video VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", rows)
-c.execute("SELECT * FROM Digital_Video")
-print((c.fetchmany(1)))
+c.execute("SELECT * FROM Digital_Video WHERE Actual_Spending='1,215'")
+print((c.fetchone()))
 
-
-conn.commit()
+#conn.commit()
 conn.close()
